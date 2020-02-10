6ee2d8c24892de89fd9e4ca42afd193cb4474463
[zzuluagarend] <zzuluagar@gmail.com>
2020-02-10 01:33:26 +0100

    styling

--- builder.js
+++ builder.js
@@ -41,7 +41,7 @@ var surveyJSON = {
     {
      "type": "radiogroup",
      "name": "activity_disctractor",
-     "indent": 10,
+     "indent": 0,
      "titleLocation": "hidden",
      "choices": [
       "0",
@@ -63,7 +63,7 @@ var surveyJSON = {
     {
      "type": "radiogroup",
      "name": "control_imc",
-     "indent": 20,
+     "indent": 0,
      "isRequired": true,
      "titleLocation": "hidden",
      "choices": [
@@ -110,7 +110,7 @@ var surveyJSON = {
    "elements": [
     {
      "type": "html",
-     "name": "question2",
+     "name": "ins_news_negative",
      "html": "</html>\n</br>\n</br>\nNow, you will see below a random news about one social topic. Please carefully read the whole piece before continue.</br>\n\n</html>"
     },
     {
@@ -128,8 +128,8 @@ var surveyJSON = {
      "titleLocation": "hidden",
      "items": [
       {
-       "name": "text1",
-       "title": "What was the rate of transactions per second mention in the text? Please input only the number"
+       "name": "distractor_negative",
+       "title": "What was the rate of transactions per second mentioned in the text? Please input only the number"
       }
      ]
     }
@@ -139,12 +139,12 @@ var surveyJSON = {
    "elements": [
     {
      "type": "html",
-     "name": "question7",
+     "name": "ins_news_positive",
      "html": "</html>\n</br>\n</br>\nNow, you will see below a random news about one social topic. Please carefully read the whole piece before continue.</br>\n\n</html>"
     },
     {
      "type": "html",
-     "name": "question10",
+     "name": "positive_news",
      "html": "</html>\n<h1><center>Bitcoin buster? The Future of Global Economy</center></h1>\n</br>\n<p>After a decade since the first cryptocurrency–digital tokens known as cryptos for short–saw the light of day, more than 2,000 different cryptos are being traded on about 190 exchange platforms around the world.\n</br>\n</br>\nCryptos have provided investors with an attractive method of decentralized currencies, with billions being traded every day. And most importantly, they are not affected by volatile politics or global banking crises.\n</br>\n</br>\nSince their launch, every crypto has seen increased its value, sometimes by 1000 times. Price fluctuations now seem to be a problem of the past thanks to a growing number of crypto start-ups that are developing alternatives known as stablecoins, digital tokens pegged to traditional fiat currencies such as the US dollar, British pound or Japanese yen. As expressed by the crypto finance company Circle, stable coins have increased investor confidence in cryptos.\n</br>\n</br>\n\"Imagine a US dollar coin that you can make payments with, use on crypto networks, or use in smart contracts to pay dividends, but which you can convert back to fiat currency at any time,\" explains Circle chief executive Jeremy Allaire. \"We're going to see a lot of businesses using tokens for revenue raising. And eventually central banks will want to do this,\" maintains Mr. Allaire.\n</br>\n</br>\nThe stability of a currency is highly praised as it impacts directly on the trust of investors and, consequently, acquisitive power. For cryptos, this has led to the tokenization of assets such as automobiles, companies and real state. Most people in the sector agree that crypto's ability to act as a smart contract presents huge opportunities to value physical assets. In countries with unstable economies, cryptos have offered an alternative for people to take part in a reliable transaction system and set their assets on currencies easily available and more reliable than those backed by local governments.\n</br>\n</br>\n\"Say I have a house in downtown Saigon worth $1m,\" says Kory Hoang, founder of Stably. \"I could tokenize it and sell those tokens on the global crypto market. You could issue a billion pieces of your house if you wanted. Then people would start trading them, just like shares in a real estate investment trust. It gives investors around the world access to investment opportunities they never had before.\"\n</br>\n</br>\nAlthough work is still afoot, strategies as satablecoins show how adaptive is the crypto system, responding quickly to changes and investors' concerns. Future looks promising for cryptos setting them as a real update to the global economic system. Let us see what will happen.\n</p>\n</html>"
     },
     {
@@ -172,7 +172,7 @@ var surveyJSON = {
      "rows": [
       {
        "value": "ques_1",
-       "text": "Which company is not mention in the text?"
+       "text": "Which company is not mentioned in the text?"
       }
      ]
     }
@@ -183,7 +183,7 @@ var surveyJSON = {
    "elements": [
     {
      "type": "html",
-     "name": "question3",
+     "name": "ioed_inst",
      "html": "<!DOCTYPE html>\n<html>\n<body>\n\n<h1><center>Rating Scale Instructions</center></h1>\n<br />\n<br />\n<p>In the next section, I will ask you to rate how well you understand certain topics. You will make your ratings on a 7-point scale where 1 means “vague understanding” and 7 means “complete understanding”. Before starting, these introductions will try to make clear what different scores on the scale are supposed to reflect. On next screens, you will see different levels of understanding of how climate change works and how human activity has contributed to it.\n<br />\n<br />Please read each explanation level in order to understand how to use the rating scale. As you will see, a 7 implies detailed and deep knowledge of the impacts of human activity on climate change. A 1 implies very little knowledge and a 4 is in the middle.\n</p>\n</body>\n</html>\n"
     }
    ]
@@ -194,7 +194,7 @@ var surveyJSON = {
     {
      "type": "html",
      "name": "7_understanding",
-     "html": "<!DOCTYPE html>\n<html>\n<body>\n<p>\n<h1><center>Rating Scale Instructions</center></h1>\n</br>\n<b>Level 7 of knowledge: </b>A person with level 7 of knowledge has a deeply and detailed knowledge of how global warming occurs and what role human activity has played on accelerating it. For instance, she will know that Earth's normal temperature is a natural process that follows from the Earth’s absorption of sunlight. As the Earth transforms sunlight’s energy into infrared light, greenhouse gases (methane, carbon dioxide, etc.) in the air absorb that energy causing the atmosphere to heat up.\n<br />\n<br />\nShe will also know that the warm atmosphere emits more infrared light causing it to be re-absorbed several times before eventually returns to space, keeping the Earth warm. The longer the infrared light stays in the atmosphere, the warmer the water and ground will get.\n<br />\n<br />It will be clear for her that since the industrial age, human activities like energy, food, and goods production has increased the amount of methane and carbon dioxide released to the atmosphere. Such increase causes an extra absorption of infrared light that slows the rate at which sunlight’s energy escapes to space. As more infrared light is captured, the average temperature rises contributing to the global warming. \n</p>\n<br />\n<br />\n</body>\n</html>\n"
+     "html": "<!DOCTYPE html>\n<html>\n<body>\n<p>\n<h1><center>Rating Scale Instructions</center></h1>\n</br>\n<b>Level 7 of knowledge: </b>A person with level 7 of knowledge has a deeply and detailed knowledge of how global warming occurs and what role human activity has played on accelerating it. For instance, she will know that Earth's normal temperature is a natural process that follows from the Earth’s absorption of sunlight. As the Earth transforms sunlight’s energy into infrared light, greenhouse gases (methane, carbon dioxide, etc.) in the air absorb that energy causing the atmosphere to heat up.\n<br />\n<br />\nShe will also know that the warm atmosphere emits more infrared light causing it to be re-absorbed several times before eventually returns to space, keeping the Earth warm. The longer the infrared light stays in the atmosphere, the warmer the water and ground will get.\n<br />\n<br />It will be clear for her that since the industrial age, human activities like energy, food, and goods production has increased the amount of methane and carbon dioxide released to the atmosphere. Such increase causes an extra absorption of infrared light that slows the rate at which sunlight’s energy escapes to space. As more infrared light is captured, the average temperature rises contributing to the global warming.\n</p>\n</body>\n</html>\n"
     }
    ]
   },
@@ -203,7 +203,7 @@ var surveyJSON = {
    "elements": [
     {
      "type": "html",
-     "name": "question6",
+     "name": "4_understanding",
      "html": "<!DOCTYPE html>\n<html>\n<body>\n<p>\n\n<h1><center>Rating Scale Instructions</center></h1>\n</br>\n<b>Level 4 of knowledge: </b>A person with level 4 of knowledge has a general idea of how climate change occurs and how human activities affect it, although without deep or detailed understanding. For instance, she might know that climate change occurs by means of greenhouse gases that somehow absorb solar energy, heating up the Earth.\n<br />\n<br />\nShe might also know that the bigger the amount of greenhouse gases released to the atmosphere, the warmer the Earth will get. It would be clear to her that human activity boosts this process causing higher temperatures. \n</p>\n</body>\n</html>\n"
     }
    ]
@@ -213,7 +213,7 @@ var surveyJSON = {
    "elements": [
     {
      "type": "html",
-     "name": "question8",
+     "name": "1_understanding",
      "html": "<!DOCTYPE html>\n<html>\n<body>\n<p>\n\n<h1><center>Rating Scale Instructions</center></h1>\n</br>\n<b>Level 1 of knowledge: </b>A person with level 1 of knowledge would probably have a very superficial understanding of global warming. She might know that fossil fuels cause the Earth's temperature to increase but how this occurs, and by which means (e.g., infrared light, greenhouse gases) is unknown to her. \n</p>\n</body>\n</html>\n"
     }
    ]
@@ -221,7 +221,7 @@ var surveyJSON = {
   {
    "name": "ioed_pre_ratings",
    "elements": [
-    {
+	{
      "type": "html",
      "name": "instructions_ioed_pre",
      "html": "<!DOCTYPE html>\n<html>\n<body>\n<p>\n\n<h1><center>Rating Scale</center></h1>\n</br>\nWith the climate change example in mind, please rate your understanding on the topics below using the 7-point scale you just read. \n</p>\n</body>\n</html>\n"
@@ -275,14 +275,14 @@ var surveyJSON = {
   {
    "name": "explanation_ioed",
    "elements": [
-    {
+	{
      "type": "html",
-     "name": "question9",
+     "name": "explanation_ins",
      "html": "<!DOCTYPE html>\n<html>\n<body>\n<p>\nNow, I would like to probe your knowledge more closely on one of the topics you rated in before. Please describe all the details you know about cryptocurrencies, going from the first step to the last, providing causal connections between steps. That is, your explanation should reflect precisely how each step causes the next step in one continuous chain from start to finish (e.g. how cryptocurrencies are created, distributed, exchanged, etc.). In other words, try to tell as complete a story as you can, with no gaps.\n</br>\n</br>\nPlease take your time, as we expect your best explanation.</p>\n</br>\n</br>\n<p style=\"font-size:18px\">How does the cryptocurrency system work?</p>\n</body>\n</html>\n"
     },
-    {
+	{
      "type": "comment",
-     "name": "question5",
+     "name": "explanation_textbox",
      "isRequired": true,
      "titleLocation": "hidden"
     }
@@ -333,7 +333,7 @@ var surveyJSON = {
   {
    "name": "attitude_scale",
    "elements": [
-    {
+	{
      "type": "html",
      "name": "attitude_inst",
      "html": "In this section, please rate the items in the table below indicating the extend to which you agree or disagree to each of the topics. For example, if you totally agree with the subject, please place a \"7\" (totally agree) on the likert scale next to it. On the contrary, if you totally disagree with the subject, please place a \"1\" (totally disagree) on the likert scale next to it. In case you don't hold an opinion on the topic, please place a \"4\" (uncertain) on the likert scale. "
